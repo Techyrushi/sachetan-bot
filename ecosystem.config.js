8f1f6274515f5f54e499bf7acf49060f7eb65ddf
root <root@srv1197877.hstgr.cloud>
2026-01-29 06:07:24 +0000

    new commit

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "conversational-agent",
+      script: "server.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
+
